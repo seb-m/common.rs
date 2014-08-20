64ff65eed35bfc5fa9f068a57d0cb01b7f7ba16d
Sebastien Martini <seb@dbzteam.org>
2014-08-21 01:15:50 +0200

    Rename macros

--- src/macros.rs
+++ src/macros.rs
@@ -3,26 +3,26 @@
 
 
 #[macro_export]
-macro_rules! try_none(
+macro_rules! try_ok_opt(
     ($e:expr) => (match $e { Ok(e) => e, Err(_) => return None })
 )
 
 #[macro_export]
-macro_rules! try_unit(
+macro_rules! try_ok_unit(
     ($e:expr) => (match $e { Ok(e) => e, Err(_) => return Err(()) })
 )
 
 #[macro_export]
-macro_rules! try_err(
+macro_rules! try_some_err(
     ($e:expr) => (match $e { Some(e) => e, None => return Err(()) })
 )
 
 #[macro_export]
-macro_rules! try_option(
+macro_rules! try_some(
     ($e:expr) => (match $e { Some(e) => e, None => return None })
 )
 
 #[macro_export]
-macro_rules! try_opt_bool(
+macro_rules! try_some_bool(
     ($e:expr) => (match $e { Some(e) => e, None => return false })
 )
